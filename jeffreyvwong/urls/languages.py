b11f26bab1c0e3648de420f7fa6918ec5e00dfaf
Jeffrey Wong <jvwong@outlook.com>
2014-09-15 17:43:45 -0400

    cleanup

--- jeffreyvwong/urls/languages.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import *
-from jeffreyvwong.models import Language 
- 
-urlpatterns = patterns('',
-    #url(r'^$', TemplateView.as_view(template_name = 'jeffreyvwong/learning.html'), name='jeffreyvwong_learning'),
-)
-
-
